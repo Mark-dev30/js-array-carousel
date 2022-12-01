2cfe1188b39732331d9c197f566808a3dda7ef75
Mark-dev30 <fronterremarco@gmail.com>
2022-12-01 15:37:22 +0100

    add -- prev

--- js/script.js
+++ js/script.js
@@ -43,11 +43,13 @@ next.addEventListener('click', function(){
     //incremento il suo valore di 1
     //aggiungere la class active al nuovo elemento dell'array items e la vado a rimuovere da quello precedente
     //stessa cosa per i cerchi
+    
     items[itemActive].classList.remove('active');
     circles[itemActive].classList.remove('active')
     itemActive++
     items[itemActive].classList.add('active');
     circles[itemActive].classList.add('active')
+    
 });
 
 prev.addEventListener('click', function(){
@@ -57,7 +59,7 @@ prev.addEventListener('click', function(){
     //stessa cosa per i cerchi
     items[itemActive].classList.remove('active');
     circles[itemActive].classList.remove('active')
-    itemActive++
+    itemActive--
     items[itemActive].classList.add('active');
     circles[itemActive].classList.add('active')
 })
